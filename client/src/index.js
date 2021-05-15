675e3c461078183d17a358aba2ed6fc1559915d1
Bhavika Joshi <pandya.bhavika@gmail.com>
2021-05-15 11:43:20 +0530

    implemented redux-form for creating stream and created json-server api

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducers from './reducers';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk  from 'redux-thunk';
+
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
-ReactDom.render(
-<Provider store={store}>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-</Provider>,
-document.querySelector("#root"));
+  </Provider>,
+  document.querySelector('#root')
+);
